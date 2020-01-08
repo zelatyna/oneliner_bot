04f73668face2ef506e7d3183eb2c53d3fd1f15a
zelatyna <magda.sendal@gmail.com>
2020-01-08 18:06:39 +0100

    change auth method to use token

--- conversationbot.py
+++ conversationbot.py
@@ -18,6 +18,8 @@ import logging
 import datetime
 import os, sys
 import json
+import sqlite3
+from sqlite3 import Error
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton)
 from telegram.ext import (
     Updater, CommandHandler, MessageHandler, Filters,
@@ -33,52 +35,63 @@ logger = logging.getLogger(__name__)
 
 AUTH, START_DATE, DATE, PHOTO, LOCATION, INFO, PARSE = range(7)
 TOKEN = os.getenv("TOKEN")
-
+DB_FILE = os.getenv("DB_FILE")
 
 def start(update, context):
     # first authenticate the user
     kb = KeyboardButton(text='Share my phone number', request_contact=True)
-    if 'phone_number' not in context.user_data or 'ol_client' not in context.user_data:
+    if 'token' not in context.user_data:
         intro = 'Hi! My name is Professor Freud! I need to authenticate you first with One Liner app.'
         # update.message.reply_text(intro, )
         update.message.reply_text(intro, reply_markup=ReplyKeyboardMarkup(keyboard=[[kb], ["Cancel"]],
                                                                           one_time_keyboard=True))
 
-        #TODO:
-        #handle reply
+        # TODO:
+        # handle reply
         return AUTH
 
     else:
         return START_DATE
 
 
-def start_date(update, context):
-    reply_keyboard = [['Today', 'Yesterday']]
-    update.message.reply_text(
-        'Please tel me what happened... ?'
-        'Use /other to type the date.\n\n',
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    return conn
 
-    return DATE
+
+def get_token(conn, username):
+    SQL = f"select a.key from authtoken_token a " \
+          f"join one_liner_customuser c on c.id = a.user_id where c.username=\"{username}\";"
+    cur = conn.cursor()
+    cur.execute(SQL)
+
+    rows = cur.fetchall()
+    return rows[0][0]
 
 
 def auth(update, context):
     contact = update.effective_message.contact
+    token = None
     reply_keyboard = [['Cool!']]
-    username = update.message.from_user.first_name
-    phone_number = contact.phone_number
-    password = phone_number
+    phone_number = "+{0}".format(contact.phone_number)
+    conn = create_connection(DB_FILE)
     try:
         ol_client = OneLiner_client()
         if ol_client:
-
-            token = ol_client.fetch_token(data={
-                'username': username,
-                "password": password
+            user_data = ol_client.get_user(data={
+                "phone_number": phone_number
             })
-            logger.info(token)
-            context.user_data['ol_client'] = ol_client
-            if ol_client.is_auth():
+            logger.info(user_data)
+            username = user_data['username']
+            token = get_token(conn, username)
+            if token is not None:
+                context.user_data['token'] = token
                 update.message.reply_text('You were successfully authenticated!',
                                           reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
             return START_DATE
@@ -86,6 +99,16 @@ def auth(update, context):
         update.message.reply_text('Your authentication was not successful! Try again :)',
                                   reply_markup=ReplyKeyboardRemove())
         return AUTH
+
+
+def start_date(update, context):
+    reply_keyboard = [['Today', 'Yesterday']]
+    update.message.reply_text(
+        'Do you want update for today or yesterday?\n'
+        'Use /other to type another date.\n\n',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return DATE
 
 
 def day_option_to_date(day):
@@ -121,51 +144,31 @@ def day(update, context):
     date = day_option_to_date(update.message.text)
     logger.info("Ok %s. Your day chosen is %s", user, date)
     context.user_data['date_pub'] = date.strftime(API_DATE_FORMAT)
-    update.message.reply_text('Thanks! I am sure it was amazing day! Write your one liner update ',
+    update.message.reply_text('Thanks! I am sure it was amazing day! Now, send me a picture, '
+                              'or send /skip if you don\'t want to. ',
                               reply_markup=ReplyKeyboardRemove())
+
+    return PHOTO
+
+
+def photo(update, context):
+    user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text('Gorgeous! Now, send me a picture to be attached to your update, '
+                              'or send /skip if you don\'t want to.')
 
     return INFO
 
 
-#
-# def photo(update, context):
-#     user = update.message.from_user
-#     photo_file = update.message.photo[-1].get_file()
-#     photo_file.download('user_photo.jpg')
-#     logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
-#     update.message.reply_text('Gorgeous! Now, send me your location please, '
-#                               'or send /skip if you don\'t want to.')
-#
-#     return LOCATION
-#
-#
-# def skip_photo(update, context):
-#     user = update.message.from_user
-#     logger.info("User %s did not send a photo.", user.first_name)
-#     update.message.reply_text('I bet you look great! Now, send me your location please, '
-#                               'or send /skip.')
-#
-#     return LOCATION
-#
-#
-# def location(update, context):
-#     user = update.message.from_user
-#     user_location = update.message.location
-#     logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
-#                 user_location.longitude)
-#     update.message.reply_text('Maybe I can visit you sometime! '
-#                               'At last, tell me something about yourself.')
-#
-#     return INFO
-#
-#
-# def skip_location(update, context):
-#     user = update.message.from_user
-#     logger.info("User %s did not send a location.", user.first_name)
-#     update.message.reply_text('You seem a bit paranoid! '
-#                               'At last, tell me something about yourself.')
-#
-#     return INFO
+def skip_photo(update, context):
+    user = update.message.from_user
+    logger.info("User %s did not send a photo.", user.first_name)
+    update.message.reply_text('I bet you look great! Now, send me your update for the day '
+                              'or send /skip.')
+
+    return INFO
 
 
 
@@ -174,13 +177,13 @@ def publish_one_liner(update, context):
     user = update.message.from_user
     logger.info("Update of %s: %s", user.first_name, update.message.text)
     context.user_data['one_liner_txt'] = update.message.text
-    context.user_data['user_name'] = user.first_name
-    ol_client = context.user_data['ol_client']
-    r = ol_client.post_one_liner(context.user_data)
+    # context.user_data['user_name'] = user.first_name
+    ol_client = OneLiner_client();
+    r = ol_client.post_one_liner(context.user_data, context.user_data['token'])
     if r.status_code != 201:
         update.message.reply_text('Ooops Something went wrong: %s' % r.text)
     else:
-        update.message.reply_text('Thank you! I hope we can talk again some day.')
+        update.message.reply_text('Thank you! Let\'s catch up tomorrow')
 
     return ConversationHandler.END
 
@@ -220,11 +223,8 @@ def main():
             DATE: [MessageHandler(Filters.regex('Today|Yesterday'), day),
                    CommandHandler('other', other_day)],
 
-            # PHOTO: [MessageHandler(Filters.photo, photo),
-            #         CommandHandler('skip', skip_photo)],
-            #
-            # LOCATION: [MessageHandler(Filters.location, location),
-            #            CommandHandler('skip', skip_location)],
+            PHOTO: [MessageHandler(Filters.photo, photo),
+                    CommandHandler('skip', skip_photo)],
 
             INFO: [MessageHandler(Filters.text, publish_one_liner)],
 
@@ -236,7 +236,7 @@ def main():
 
     dp.add_handler(conv_handler)
 
-    #dp.add_handler(MessageHandler(Filters.contact, contact_callback))
+    # dp.add_handler(MessageHandler(Filters.contact, contact_callback))
     # log all errors
     dp.add_error_handler(error)
 
--- oneliner_api.py
+++ oneliner_api.py
@@ -7,30 +7,31 @@ API_DATE_FORMAT = '%Y-%m-%d'
 class OneLiner_client:
     def __init__(self, host='http://localhost:8000/one_liner'):
         self.host = host
-        self.token = None
+        self.headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
 
-    def is_auth(self):
-        return self.token is not None
-
-    def fetch_token(self, data):
-        url = self.host + '/api-token-auth/'
-        r = requests.post(url, json=data)
-        if r.status_code != 200:
-            logging.error(r.text)
+    def get_user(self, data):
+        user_data ={}
+        url = self.host + '/users/'
+        params = {}
+        if 'phone_number' in data.keys():
+            params = ({'phone_number' : data['phone_number']})
+        req = requests.get(url=url, params=params, headers=self.headers)
+        if req.status_code != 200:
+            logging.error(req.text)
         else:
-            r_json = r.json()
-            self.token = r_json['token']
+            req_json = req.json()
+            if req_json['count'] ==1:
+                user_data = req_json['results'][0]
+                self.username = user_data['username']
+        return user_data
 
-        return self.token
 
-
-    def post_one_liner(self, data):
+    def post_one_liner(self, data, token):
         headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-        headers['Authorization'] = "Token {token}".format(token=self.token)
+        headers['Authorization'] = "Token {token}".format(token=token)
         post_data = {
             "one_liner_text": data['one_liner_txt'],
-            "pub_date": data['date_pub'],
-            "user_id": 2
+            "pub_date": data['date_pub']
         }
 
 

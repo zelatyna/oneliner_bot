ca51da5b09b3ac7ed45ebaf60877ace94db20482
zelatyna <magda.sendal@gmail.com>
2020-01-21 17:45:08 +0100

    remove code from another example

--- conversationbot.py
+++ conversationbot.py
@@ -251,23 +251,5 @@ def main():
 
     updater.idle()
 
-
-
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
-    return "!", 200
-
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-
 if __name__ == '__main__':
     main()
